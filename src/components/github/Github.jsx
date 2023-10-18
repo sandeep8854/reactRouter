9edad94c4a3338991104e66d293184744bddb54f
Sandeep Prajapati <sandeep.prajapati.23.2@mountblue.tech>
2023-10-18 23:24:31 +0530

    adding all file at the time aafter completing the project

---
+++ src/components/github/Github.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  // with loader
+  const data = useLoaderData();
+
+  // without loader.
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/sandeep8854")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Number of Github followers : {data.followers}
+      <img
+        src={data.avatar_url}
+        alt="image not found"
+        width={300}
+        className="mx-auto p-2"
+      />
+    </div>
+  );
+}
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/sandeep8854");
+  return response.json();
+};
---
+++ src/main.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Layout from "./Layout";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import User from "./components/user/User";
+import Github, { githubInfoLoader } from "./components/github/Github";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "",
+//         element: <Home />,
+//       },
+//       {
+//         path: "about",
+//         element: <About />,
+//       },
+//       {
+//         path: "contact",
+//         element: <Contact />,
+//       },
+//     ],
+//   },
+// ]);
+
+// Type 2 for implement react router .
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="user/:userid" element={<User />} />
+      {/* <Route path="github" element={<Github />} /> */}
+
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
